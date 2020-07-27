a40056ff6ab9e1c7ab00928f10ac7e3dd2992d25
Hazel <hazel@edibly.ca>
2020-07-27 17:09:17 -0600

    TOS for sociables

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home  from './components/Home';
-import About  from './components/About';
-import Contact  from './components/Contact';
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Sociables from "./components/Sociables";
 
 function App() {
   return (
-
-    <div  style={{ overflow:'auto'}}>
+    <div style={{ overflow: "auto" }}>
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
           {/* <Route component={NoMatch} /> */}
+          <Route path="/sociables-tos-pp" component={Sociables} />
         </Switch>
       </Router>
-      </div>
-
+    </div>
   );
 }
 
